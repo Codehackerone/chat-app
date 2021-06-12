915dc755404cd9102e23ec533de3290a510a602e
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-12 09:27:28 +0530

    basic io server initialized

--- build/server.js
+++ build/server.js
@@ -5,10 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = require("dotenv");
-var app = express_1["default"]();
+var path_1 = __importDefault(require("path"));
+var cors_1 = __importDefault(require("cors"));
+var http_1 = __importDefault(require("http"));
+var socket_io_1 = require("socket.io");
 dotenv_1.config();
+var app = express_1["default"]();
+var server = http_1["default"].createServer(app);
+var io = new socket_io_1.Server(server);
+app.use(cors_1["default"]());
+app.use(express_1["default"].json());
+app.use(express_1["default"].urlencoded({ extended: true }));
+app.use(express_1["default"].static(path_1["default"].join(__dirname, 'public')));
+app.use(express_1["default"].static(__dirname + "/views"));
+app.set("view engine", "ejs");
+io.on('connection', function (socket) {
+    console.log("User connected");
+});
 var port = Number(process.env.PORT);
-app.listen(port, function () {
+server.listen(port, function () {
     console.log("Chat-app running on port " + port + ".");
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,11 +1,27 @@
 import express from "express";
 import { config } from "dotenv";
-
-const app = express();
+import path from "path";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 config();
+const app = express();
+const server=http.createServer(app);
+const io=new Server(server);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+
+io.on('connection',socket=>{
+    console.log("User connected");  
+})
 
 const port = Number(process.env.PORT);
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Chat-app running on port ${port}.`);
 });
