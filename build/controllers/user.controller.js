9c27d4a6d4bea164f38190667a40a0f9053cc0a2
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-17 09:26:03 +0530

    custom signin with google added

--- build/controllers/user.controller.js
+++ build/controllers/user.controller.js
@@ -3,15 +3,15 @@ exports.__esModule = true;
 exports.googleauth = exports.signin = exports.renderSignin = void 0;
 var redirectURI = "/users/googleauth";
 var renderSignin = function (req, res) {
-  res.render("signin");
+    res.render("signin");
 };
 exports.renderSignin = renderSignin;
 var signin = function (req, res) {
-  res.send("Google Login");
+    res.send("Google Login");
 };
 exports.signin = signin;
 var googleauth = function (req, res) {
-  res.send("Google Login Success");
+    res.send("Google Login Success");
 };
 exports.googleauth = googleauth;
 //# sourceMappingURL=user.controller.js.map
--- public/js/oauth.js
+++ public/js/oauth.js
@@ -1,8 +1,5 @@
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log("User signed out.");
-  });
+function onFailure(error) {
+  console.log(error);
 }
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
@@ -10,4 +7,21 @@ function onSignIn(googleUser) {
   console.log("Name: " + profile.getName());
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail());
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSignIn,
+    'onfailure':onFailure
+  });
+}
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+  });
 }
