9d15506422716b9626895a682cd4fb2909d9a9ec
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-17 10:34:27 +0530

    verify functions addee

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,12 +1,28 @@
 const redirectURI = "/users/googleauth";
 import { OAuth2Client } from "google-auth-library";
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export const renderSignin = (req: any, res: any) => {
   res.render("signin");
 };
 
+const verify=async(token:any)=>{
+  const ticket=await client.verifyIdToken({
+    idToken:token,
+    audience:process.env.GOOGLE_CLIENT_ID,
+  })
+  const payload=ticket.getPayload();
+  const userid=payload['sub'];
+}
+
 export const signin = (req: any, res: any) => {
-  res.send("Google Login");
+  let token=req.body.token;
+  verify(token)
+    .then(()=>{
+      res.cookie('session-token',token);
+      res.send('success');
+    })
+    .catch(console.error);
 };
 
 export const googleauth = (req: any, res: any) => {
