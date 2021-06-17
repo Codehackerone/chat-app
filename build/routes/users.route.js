0ef160db5c1df94f56a129bd6db46c819f975536
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-17 12:19:05 +0530

    updated .env.example file

--- build/routes/users.route.js
+++ build/routes/users.route.js
@@ -1,24 +1,14 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var user_controller_1 = require("../controllers/user.controller");
 var auth_middleware_1 = require("../middlewares/auth.middleware");
 var Router = express_1["default"].Router();
-Router.route("/signin")
-  .get(user_controller_1.renderSignin)
-  .post(user_controller_1.signin);
-Router.route("/profile").get(
-  auth_middleware_1.authorize(),
-  user_controller_1.profile
-);
-Router.route("/signout").all(
-  auth_middleware_1.authorize(),
-  user_controller_1.signout
-);
+Router.route("/signin").get(user_controller_1.renderSignin).post(user_controller_1.signin);
+Router.route("/profile").get(auth_middleware_1.authorize(), user_controller_1.profile);
+Router.route("/signout").all(auth_middleware_1.authorize(), user_controller_1.signout);
 exports["default"] = Router;
 //# sourceMappingURL=users.route.js.map
--- build/server.js
+++ build/server.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = require("dotenv");
@@ -28,65 +26,47 @@ app.use("/js", express_1["default"].static("public/js"));
 app.use("/img", express_1["default"].static("public/images"));
 app.set("view engine", "ejs");
 io.on("connection", function (socket) {
-  socket.on("joinRoom", function (_a) {
-    var username = _a.username,
-      room = _a.room;
-    var user = users_1.userJoin(socket.id, username, room);
-    socket.join(user.room);
-    socket.emit(
-      "message",
-      messages_1.formatMessage(botName, "Welcome to Chatversity!")
-    );
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        "message",
-        messages_1.formatMessage(
-          botName,
-          user.username + " has joined the chat"
-        )
-      );
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: users_1.getRoomUsers(user.room),
+    socket.on("joinRoom", function (_a) {
+        var username = _a.username, room = _a.room;
+        var user = users_1.userJoin(socket.id, username, room);
+        socket.join(user.room);
+        socket.emit("message", messages_1.formatMessage(botName, "Welcome to Chatversity!"));
+        socket.broadcast
+            .to(user.room)
+            .emit("message", messages_1.formatMessage(botName, user.username + " has joined the chat"));
+        io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            users: users_1.getRoomUsers(user.room)
+        });
     });
-  });
-  socket.on("chatMessage", function (msg) {
-    var user = users_1.getCurrentUser(socket.id);
-    io.to(user.room).emit(
-      "message",
-      messages_1.formatMessage(user.username, msg)
-    );
-  });
-  socket.on("disconnect", function () {
-    var user = users_1.userLeave(socket.id);
-    if (user) {
-      io.to(user.room).emit(
-        "message",
-        messages_1.formatMessage(botName, user.username + " has left the chat")
-      );
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        users: users_1.getRoomUsers(user.room),
-      });
-    }
-  });
+    socket.on("chatMessage", function (msg) {
+        var user = users_1.getCurrentUser(socket.id);
+        io.to(user.room).emit("message", messages_1.formatMessage(user.username, msg));
+    });
+    socket.on("disconnect", function () {
+        var user = users_1.userLeave(socket.id);
+        if (user) {
+            io.to(user.room).emit("message", messages_1.formatMessage(botName, user.username + " has left the chat"));
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: users_1.getRoomUsers(user.room)
+            });
+        }
+    });
 });
 app.get("/", function (req, res) {
-  res.render("index");
+    res.render("index");
 });
 app.get("/chat", function (req, res) {
-  var _a = req.query,
-    username = _a.username,
-    room = _a.room;
-  res.render("chat", { username: username, room: room });
+    var _a = req.query, username = _a.username, room = _a.room;
+    res.render("chat", { username: username, room: room });
 });
 app.use("/users", users_route_1["default"]);
 app.all("*", function (req, res) {
-  res.send("Sorry! Route not found");
+    res.send("Sorry! Route not found");
 });
 var port = Number(process.env.PORT);
 server.listen(port, function () {
-  console.log("Chat-app running on port " + port + ".");
+    console.log("Chat-app running on port " + port + ".");
 });
 //# sourceMappingURL=server.js.map
