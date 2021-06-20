5abf94f03aca62a208f7f388575678d3b9336621
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-20 19:17:38 +0530

    validator middleware added

---
+++ middlewares/validator.middlewares.ts
@@ -0,0 +1,13 @@
+import { userSchema_base } from "../helpers/schemas";
+
+export const validateUser_base = () => {
+    return async (req:any, res:any, next:any) => {
+        const { error } = userSchema_base.validate(req.body);
+        if (error) {
+            const msg = error.details.map((el:any) => el.message).join(",");
+            res.send(msg);
+        } else {
+            next();
+        }
+    };
+};
