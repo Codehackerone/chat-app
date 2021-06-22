d01c90ccaa222de14ec1a1a848019cfc07bd76c4
Codehackerone <soumyajitdatta123@gmail.com>
2021-06-22 12:16:12 +0530

    room model added

---
+++ models/room.model.ts
@@ -0,0 +1,31 @@
+import { Schema,model } from 'mongoose';
+
+const roomSchema = new Schema(
+    {
+        _id: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        name: {
+            type: String,
+            required:true,
+        },
+        type:{
+            type: String,
+            enum:['dual','group']
+        },
+        users:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:'User'
+            }
+        ]
+    },
+    {
+        timestamps:true,
+    }
+)
+
+let Room = model('Room', roomSchema);
+
+export default Room;
