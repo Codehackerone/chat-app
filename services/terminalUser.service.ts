10ae468ab23a408be1e41ea767b6b6ffa325db58
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-15 01:23:57 +0530

    controller added

--- services/terminalUser.service.ts
+++ services/terminalUser.service.ts
@@ -1,7 +1,7 @@
 import terminalUser from "../models/terminalUser";
 import bcrypt from "bcryptjs";
 
-export const registerService:Object=async(username:String, password:String)=>{
+export const registerService:any=async(username:String, password:String)=>{
     let hashedPassword:String=bcrypt.hashSync(String(password), 12);
     let user:any=await terminalUser.create({
         username,
@@ -10,7 +10,7 @@ export const registerService:Object=async(username:String, password:String)=>{
     return user;
 }
 
-export const loginService:Object=async(username:String, password:String)=>{
+export const loginService:any=async(username:String, password:String)=>{
     let user:any = await terminalUser.findOne({username:username});
     if(!user)throw "invalid username entered";
     if(!bcrypt.compareSync(password, user.password))throw "incorrect password";
