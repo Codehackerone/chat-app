95f9cd38e72e2463c898b8c814967a6246b99b9e
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-15 01:01:36 +0530

    terminalUser service added

--- models/terminalUser.ts
+++ models/terminalUser.ts
@@ -1,5 +1,4 @@
 import { Schema, model } from "mongoose";
-import bcrypt from "bcryptjs";
 
 const terminalUserSchema = new Schema({
   username: {
@@ -12,14 +11,6 @@ const terminalUserSchema = new Schema({
     required: true,
   },
 });
-
-// terminalUserSchema.pre("save", async function (next) {
-//   if (!this.isModified || !this.isNew) {
-//     next();
-//   } else this.isModified("password");
-//   if (this.password) this.password = bcrypt.hashSync(String(this.password), 12);
-//   next();
-// });
 
 let terminalUser = model("terminalUser", terminalUserSchema);
 
